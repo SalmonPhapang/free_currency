879aba5bdad426b3e1d04a3668821ebde717339c
Salmon <salmon@anylytical.co.za>
2021-11-25 23:27:55 +0200

    code clean up

--- src/components/home.component.ts
+++ src/components/home.component.ts
@@ -2,17 +2,11 @@ import Component from 'vue-class-component'
 import { Vue } from "vue-property-decorator";
 import CurrencyService from "@/service/currency.service";
 import {Currencies, ICurrencies} from "@/model/currencies.model";
-import {IData} from "@/model/Data.model";
 
 @Component
 export default class HomeComponent extends Vue {
     private currencyService : CurrencyService = new CurrencyService();
     public currencies: ICurrencies = new Currencies();
-    public  items : IData[] = [];
-    public headers:Array<any> = [
-        {text: 'Symbol', align: 'start', value: 'name'},
-        { text: 'Rate', value: 'rate' }
-    ];
     loading = true;
     mounted() : void {
         this.findCurrencies();
@@ -22,7 +16,6 @@ export default class HomeComponent extends Vue {
 
         this.currencyService.findAll()
             .then((response) =>{
-              // eslint-disable-next-line no-debugger
                 this.currencies = response;
                 this.loading = false;
             })
